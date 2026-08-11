358278911ca1abcb6104bdca63f39c9b014b7262
Joshen John Cruz <cpmalinao30@src.pshs.edu.ph>
2026-08-11 11:07:55 +0800

    Add files via upload

---
+++ 9-Lithium/Malinao/CP.py
@@ -0,0 +1,5 @@
+Input = float(input("Enter year pls:"))
+
+def Leap (Input):
+    if Input % 4 == 0 and Input != 100: 
+        print (Leap)
---
+++ 9-Lithium/Malinao/main.py
@@ -0,0 +1,24 @@
+# math_utils.py
+
+def add_numbers(a, b):
+
+    return a + b
+
+def multiply_numbers(a, b):
+
+    return a * b
+
+def greet_user(name):
+
+    return f"Hello, {name}! Welcome to VS Code."
+
+def main():
+    print(add_numbers(15, 10))
+
+    print(multiply_numbers(5, 4))
+
+    print(greet_user("Clive"))
+
+if __name__ == "__main__":
+
+    main()
